4d7d7a851afefea6a494c8f76c18454592f05ddf
Adam Thomas <thomasadam83@hotmail.com>
2020-09-21 15:58:56 -0400

    Update node-to-arduino.ino

--- node-to-arduino.ino
+++ node-to-arduino.ino
@@ -1,1 +1,33 @@
-/* * This file receives a Serial value of "1" or "0" and changes the light based on * that value.  */  // Define the port for the LEDint lightPin = 2; void setup() {     // Initialize the light pin  pinMode(lightPin, OUTPUT);    // Initialize the Serial  Serial.begin(9600);  }void loop() {    // CHeck to see if Serial data is being received  if (Serial.available() > 0) {        // Create a new string variable to receive Serial data    String receivedString = "";        // Loop through received data and append to the receivedString variable    while (Serial.available() > 0) {      receivedString += char(Serial.read ());    }        // Print received Serial data    Serial.println(receivedString);        // Change LED status based on received data    if(receivedString == "1")      digitalWrite(lightPin,HIGH);      else      digitalWrite(lightPin,LOW);      }}
+/*
+*/  
+
+int lightPin = 2;
+ 
+void setup() 
+{ 
+  
+  pinMode(lightPin, OUTPUT);
+  Serial.begin(9600);
+  
+}
+
+void loop() {
+ 
+  if (Serial.available() > 0) {
+    
+    String receivedString = "";
+    
+    while (Serial.available() > 0) {
+      receivedString += char(Serial.read ());
+    }
+    
+    Serial.println(receivedString);
+    
+    if(receivedString == "1")
+      digitalWrite(lightPin,HIGH);  
+    else
+      digitalWrite(lightPin,LOW);
+    
+  }
+
+}
